6cacbaa1a21639d5524b033696dd000124324ee7
CicakIndo <39509029+CicakIndo@users.noreply.github.com>
2019-01-27 11:28:33 +0700

    Update intro.php

--- intro.php
+++ intro.php
@@ -1,4 +1,4 @@
 <?php
 ${"GLOBALS"}["bypdfciqjz"]="intro";
-${${"GLOBALS"}["bypdfciqjz"]}= ${${"GLOBALS"}["pacrgmeqgb"]}."\n ╔═╦═╦═╗ ╔══╗ © \n ║ ║ ║ ║	║ ═╣ \n ║     ║	║ ═╣	creator : Cicak Indo \n ╚═════╝	╚══╝ 	version : v1.3 (VIP)".${${"GLOBALS"}["gcaluyzkyx"]}; 
+${${"GLOBALS"}["bypdfciqjz"]}= ${${"GLOBALS"}["pacrgmeqgb"]}."\n ╔═╦═╦═╗ ╔══╗ © \n ║ ║ ║ ║ ║ ═╣ \n ║     ║ ║ ═╣	creator : Cicak Indo \n ╚═════╝ ╚══╝ 	version : v1.3 (VIP)".${${"GLOBALS"}["gcaluyzkyx"]}; 
 ?>
